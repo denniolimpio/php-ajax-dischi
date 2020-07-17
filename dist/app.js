5d5cc3d9bec7649da102c92e066af735782f76c2
dennis <dennis.olimpio0794@gmail.com>
2020-07-17 13:04:51 +0200

    consegna milestone 1

--- dist/app.js
+++ dist/app.js
@@ -115,8 +115,8 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/dennis/Desktop/Boolean/repo/php/php-ajax-dischi/src/app.js */"./src/app.js");
-module.exports = __webpack_require__(/*! /Users/dennis/Desktop/Boolean/repo/php/php-ajax-dischi/src/app.scss */"./src/app.scss");
+__webpack_require__(/*! /Users/dennis/Desktop/Boolean/repo/php/dischi/src/app.js */"./src/app.js");
+module.exports = __webpack_require__(/*! /Users/dennis/Desktop/Boolean/repo/php/dischi/src/app.scss */"./src/app.scss");
 
 
 /***/ })
--- index.php
+++ index.php
@@ -1,8 +1,62 @@
-<?php
+<?php include  __DIR__ . '/database.php'?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+
+  <link rel="stylesheet" href="dist/app.css">
+  <title></title>
+</head>
+<body>
+
+  <header>
+
+    <div class="container">
+      <div class="logo">
+        <img src= ' img/logo.svg' alt="">
+      </div>
+
+    </div>
+
+  </header>
+
+  <main>
+
+    <!-- inizio cont  -->
+    <div class="container">
+      <!-- inizio album container  -->
+
+      <div class="album-container">
+
+        <?php foreach ($database as $album ) { ?>
+
+          <div class="album">
+            <div class="cover">
+              <img src=" <?php echo $album['poster'] ?>" alt=" cover">
+            </div>
+            <h2> <?php echo $album['title'] ?></h2>
+            <span> <?php echo $album['author'] ?></span>
+            <span> <?php  echo $album['year'] ?></span>
+          </div>
+
+
+        <?php } ?>
 
 
 
 
+      </div>
+
+      <!-- fine album container  -->
 
 
- ?>
+    </div>
+    <!-- fine cont dischi -->
+
+  </main>
+
+
+  <footer></footer>
+</body>
+</html>
